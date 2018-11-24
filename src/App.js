536a0bf9df42d8e70972a4a6cc129a5b23126cd7
wangyuandong <yuandong.wang@qq.com>
2018-11-24 19:44:31 +0800

    初步构建博客管理页面

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 import './App.css';
 
+
+import {Cascader} from 'antd';
+
+const options = [{
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [{
+        value: 'hangzhou',
+        label: 'Hanzhou',
+        children: [{
+            value: 'xihu',
+            label: 'West Lake',
+        }],
+    }],
+}, {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [{
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [{
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+        }],
+    }],
+}];
+
+function onChange(value) {
+    console.log(value);
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {text: ''} // You can also pass a Quill Delta here
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(value) {
+        this.setState({text: value})
+    }
+
+    render() {
+        return (
+            <div>
+                <Cascader style={{width: '300px'}} options={options} onChange={onChange} changeOnSelect/>
+                <ReactQuill value={this.state.text} onChange={this.handleChange}/>
+            </div>
+        );
+    }
 }
 
 export default App;
